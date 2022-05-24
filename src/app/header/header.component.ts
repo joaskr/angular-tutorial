ec7fd22c488050e669940a35d9723b9e19665e2d
jasg <joanna.skrzypczak@gft.com>
2022-05-24 10:29:04 +0200

    finished section 6

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  @Output() featureSelected = new EventEmitter<string>();
   constructor() {}
-
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
+  }
   ngOnInit(): void {}
 }
