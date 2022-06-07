573935a46ea3c078a5a09c8ba03878e8285eed99
jasg <joanna.skrzypczak@gft.com>
2022-06-07 13:01:10 +0200

    finished section 22

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import {ShoppingListService} from "./shopping-list.service";
 import {Subscription} from "rxjs";
+import {LoggingService} from "../logging.service";
 
 @Component({
   selector: 'app-shopping-list',
@@ -11,13 +12,14 @@ import {Subscription} from "rxjs";
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private igChangeSub: Subscription
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(private shoppingListService: ShoppingListService, private loggingService: LoggingService) {}
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
     this.igChangeSub = this.shoppingListService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     })
+    this.loggingService.printLog('hello from sh list comp');
   }
   onEditItem(index: number) {
     this.shoppingListService.startedEditing.next(index);
